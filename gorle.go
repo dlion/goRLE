c835553422d1cc51b76a7c76042ad21a235b5e79
Domenico Luciani <domenicoleoneluciani@gmail.com>
2015-12-02 20:11:34 +0100

    No Decoding currently

--- gorle.go
+++ gorle.go
@@ -21,24 +21,3 @@ func Encode(s string) string {
 	o.WriteString(fmt.Sprintf("%d%c", count, s[i]))
 	return o.String()
 }
-
-func Decode(s string) string {
-	var o bytes.Buffer
-	var count string
-	for i := 0; i < len(s); i++ {
-		if val, _ := strconv.Atoi(string(s[i])); val > 0 && val <= 9 {
-			if countN, err := strconv.Atoi(count); err == nil {
-				count = fmt.Sprintf("%d%d", countN, val)
-			} else {
-				count = string(s[i])
-			}
-		} else {
-			count2, _ := strconv.Atoi(count)
-			for j := 0; j < count2; j++ {
-				o.WriteString(fmt.Sprintf("%c", s[i]))
-			}
-			count = ""
-		}
-	}
-	return o.String()
-}
